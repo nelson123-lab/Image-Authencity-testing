1e6e11f0cecc4872e7f723b6d13739a4f8abcca3
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-05-23 23:26:39 -0500

    m

--- Meta_data_finder.py
+++ Meta_data_finder.py
@@ -7,4 +7,5 @@ stable_diffusion_image = Image.open('stable_test.jpg')
 metadata1 = cam_image.getexif()
 metadata2 = stable_diffusion_image.getexif()
 # Print the metadata
-print(metadata)
+print("Meta data of the image taken from camera",metadata1)
+print("Meta data of the image taken from stable diffusion image", metadata2)
