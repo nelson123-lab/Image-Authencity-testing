65a0125878a8f82e8dba2a97781077cbb9c7ceb4
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-05-23 23:34:29 -0500

    Update Meta_data_finder.py

--- Meta_data_finder.py
+++ Meta_data_finder.py
@@ -9,3 +9,11 @@ metadata2 = stable_diffusion_image.getexif()
 # Print the metadata
 print("Meta data of the image taken from camera",metadata1)
 print("Meta data of the image taken from stable diffusion image", metadata2)
+
+
+"""
+Meta data of the image taken from camera 
+{296: 2, 282: 72.0, 256: 4000, 257: 2252, 34665: 236, 271: 'samsung', 272: 'Galaxy S23 Ultra', 305: 'S918U1UES1AWC9', 274: 1, 306: '2023:05:23 19:52:19', 531: 1, 283: 72.0}
+Meta data of the image taken from stable diffusion image {}
+"""
+
